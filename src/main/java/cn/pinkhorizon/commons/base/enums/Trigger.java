d6e2cc78be776513bec9a6d78e64d5ad21bfca53
yuanlei7 <ylsn666@gmail.com>
2021-06-18 14:40:39 +0800

    a

--- src/main/java/cn/pinkhorizon/commons/base/enums/Trigger.java
+++ src/main/java/cn/pinkhorizon/commons/base/enums/Trigger.java
@@ -6,7 +6,9 @@ package cn.pinkhorizon.commons.base.enums;
  */
 public enum Trigger {
     /**
-     * 表示两种对立状态.例如：开或关、启动或停止、上架或下架、执行或不执行等等完全对立的状态
+     * 表示两种对立状态.例如
+     *      YEA：赞成、同意、启动、上架、执行等
+     *      NAY：反对、拒绝、停止、下架、取消等
      */
-    ON, OFF;
+    YEA, NAY;
 }
